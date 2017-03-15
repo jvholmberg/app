1772c13766de5ade736ebbaa05cadb0929ca569a
johan holmberg <johan@johans-MacBook-Pro.local>
2017-03-15 23:31:13 +0100

    Added jquery and bluebird as dependencies

--- app/controllers/provision.js
+++ app/controllers/provision.js
@@ -4,22 +4,42 @@ var express = require('express'),
   Provision = mongoose.model('Provision');
 
 module.exports = function(app) {
+
+  // set mongoose to use bluebird for Promises
+  mongoose.Promise = require('bluebird');
+
   app.use('/api/provision', router);
 };
 
-router.get('/all', function(req, res, next) {
-  Provision.find(function(err, data) {
-    if (err)
-      return next(err);
-    return res.json(data);
-  });
+// This should not be used within application
+
+// router.get('/all', function(req, res, next) {
+//   Provision.find(function(err, data) {
+//     if (err) { return next(err); }
+//     return res.json(data);
+//   });
+// });
+
+router.get('/search/:id', function(req, res, next) {
+  var id = req.params.id;
+
+  Provision.findById(id, function(err, data) {
+      if (err) { return next(err); }
+      return res.json(data);
+    });
 });
-router.get('/search/:query', function(req, res, next) {
-  Provision.find({
-    name: { '$regex': req.params.query, "$options": "i" }
-  }, function(err, data) {
-    if (err)
-      return next(err);
-    return res.json(data);
-  });
+
+router.get('/search/:query/:skip/:limit', function(req, res, next) {
+  var query = req.params.query,
+    skip = req.params.skip !== null ? parseInt(req.params.skip) : 0,
+    limit = req.params.limit !== null ? parseInt(req.params.limit) : 25
+
+  Provision.find({name: { '$regex': query, '$options': 'i' }})
+    .sort({'name': -1})
+    .skip(skip)
+    .limit(limit)
+    .exec(function(err, data) {
+      if (err) { return next(err); }
+      return res.json(data);
+    });
 });
